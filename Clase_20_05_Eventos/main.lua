65bb65a25e5d8e74fff41f022e58e4a8aea77463
Miguel Frade Fores <Miguel.Frade@mojix.com>
2022-05-23 20:35:07 -0400

    Adicion de transiciones para los eventos

--- Clase_20_05_Eventos/main.lua
+++ Clase_20_05_Eventos/main.lua
@@ -13,6 +13,9 @@ ch = display.contentHeight
 fondo = display.newRect( cw/2, ch/2, cw, ch )
 fondo:setFillColor( 0.45 )
 
+heroe_move_speed = 300
+
+local rotacion_icono
 
 cuadrado = display.newRect(cw/2, ch/2, 50, 50)
 
@@ -21,21 +24,41 @@ circulo = display.newCircle( cw/2, ch/4, 25 )
 redondo = display.newCircle ( cw/2, ch*0.75, 25)
 redondo:setFillColor( 0,0,1 )
 redondo.nombre = "Juan"
+redondo.alpha = 0.5
 
 rectangulo = display.newRect( cw/4, ch/2, 30, 50 )
 rectangulo:setFillColor( 1, 0, 0 )
+rectangulo.alpha=0.2
+
+icono = display.newImage("Icon.png", cw*0.75, ch/2)
+icono.rotation = 90
+icono.anchorX = 0; icono.anchorY =0 
+print("Rotacion del icono", icono.rotation)
+
+titulo = display.newText( "Victoria!", cw/2, ch/8, "arial", 20 )
+
+print("Propiedad alpha del rectangulo", rectangulo.alpha)
 
 function mover_cuadrado(event )
 	if (event.phase == "began") then
 		print( "Fase began del circulo", event.x, event.y )
+		--capturar posicion del click del mouse
+
 	elseif (event.phase == "moved") then
 		print( "Fase moved", event.x, event.y )
 
 
 	elseif( event.phase == "ended") then
 		print( "Fase del ended", event.x, event.y )
-		cuadrado.x =  redondo.x  --math.random( 0, cw  )
-		cuadrado.y =  redondo.y  -- math.random( 0, ch )
+		-- cuadrado.x =  math.random( 0, cw  )
+		-- cuadrado.y =   math.random( 0, ch )
+
+		neoX =  math.random( 0, cw  )
+		neoY =  math.random( 0, ch )
+		transition.to( cuadrado, {x=neoX, y=neoY, time=1000})
+
+		-- jugador mover(posicion del mouse)
+
 	end
 
 end
@@ -93,6 +116,8 @@ function redondo:touch(event)
 		redondo:removeEventListener( "touch", redondo )
 		redondo:addEventListener( "touch", rellenar )
 
+		transition.to(redondo, { alpha=0.2, time =500, iterations = 20 })
+
 	end
 	return true
 end
@@ -100,10 +125,29 @@ end
 function agrandar( self, e)
 
 	if e.phase == "ended" then
-		self.width = self.width *2
-		self.height = self.height *2
+		-- self.width = self.width *2
+		-- self.height = self.height *2
+
+--		transition.to(self, { xScale=self.xScale*2, yScale=2, time=800 })
+		transition.to(self, { width=self.width/2, height=self.height*3, time = 1000  })
+		transition.to(self, {x=cw, time =2000, alpha=1})
+
+
 	end
+	return true
 end
+
+function fondo:touch( e )
+	if e.phase == "ended" or e.phase == "cancelled" then 
+		-- circulo.x = e.x
+		-- circulo.y = e.y
+		transition.to(circulo, {x=e.x , y=e.y, time= heroe_move_speed })
+
+	end
+	return true
+end
+
+fondo:addEventListener( "touch", fondo )
 
 rectangulo.touch = agrandar
 
@@ -116,4 +160,36 @@ circulo:addEventListener( "touch", mover_cuadrado )
 redondo:addEventListener( "touch", redondo )
 -- redondo:addEventListener( "touch", rellenar )
 
+function icono:touch(e)
+
+	if e.phase == "ended" then
+		transition.pause(rotacion_icono)
+	end
+	return true
+end
+
+
+function detenidos()
+	print("objeto detenido")
+	icono:setFillColor( 1,0,0 )
+end
+
+function iniciar( ... )
+	icono.xScale = 2;
+	icono.yScale = 2
+end
+
+function mover(  )
+	-- body
+
+
+	transition.to(icono, {y = ch/8, time=1000, delay = 2000, onStart = iniciar})
+
+end
+
+icono:addEventListener( "touch", icono )
+
+rotacion_icono = transition.to(icono, {rotation = 720, time = 5000, onComplete = mover, onPause = detenidos, tag = "objeto"})
+
+transition.to(titulo, { size = 40, iterations=6, time =1000, tag = "objeto" })
 
