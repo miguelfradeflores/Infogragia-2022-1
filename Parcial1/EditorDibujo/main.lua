30a6c00deaeec773b323a468f3f17759ff50ed73
Jud4 <Jud4@users.noreply.github.com>
2022-05-30 12:24:12 -0400

    Requirements complete

--- Parcial1/EditorDibujo/main.lua
+++ Parcial1/EditorDibujo/main.lua
@@ -10,6 +10,14 @@ cw = display.contentWidth
 ch = display.contentHeight
 -- Colores
 gris={146/255,141/255,140/255}
+negro={0,0,0}
+rojo={1,0,0}
+amarillo={1,1,0}
+verde={0,223/255,0}
+cian={0,1,1}
+magenta={1,0,1}
+azul={0,0,1}
+
 Backcolor={1,1,1,1}
 
 local canvas = display.newRect(0,0,cw,ch)
@@ -20,7 +28,7 @@ canvas:setFillColor(unpack(Backcolor))
 local idTool = 0 
 local strok=5
 local eraStrok=30
-local baseColor={1,0,0,1}
+local baseColor={1,0,0}
 -- Grupo de dibujos
 local grp_drawings=display.newGroup()
 local moveRectP3
@@ -29,6 +37,11 @@ local moveRectP3
 menuBar = display.newRoundedRect( 20,ch/8,cw/15, (ch/8)*6,10)
 menuBar:setFillColor(146/255,141/255,140/255,0.6)
 menuBar.anchorX=0;menuBar.anchorY=0
+
+-- Paleta de colores
+colorBox = display.newRoundedRect(cw-20,10,(cw/15)*5,cw/15*1.5,10)
+colorBox:setFillColor(146/255,141/255,140/255,0.6)
+colorBox.anchorX=1;colorBox.anchorY=0
 
 iconsIm = {"pencil.png","eraser.png","square.png","dotted-circle.png","triangle.png"}
 -- menu actions
@@ -50,6 +63,25 @@ for i,v in ipairs(iconsIm) do
 	icons[i]:addEventListener("touch",icons[i])
 	icons[i].id = i
 end
+-- Paleta de colores
+function selColor(self,event)
+	if event.phase == "ended" then
+		baseColor = self.color
+		print("entrado")
+	end
+	return
+end
+local colors = {negro,rojo,amarillo,verde,azul,cian,magenta,gris}
+colorCells={}
+for i,v in ipairs(colors) do
+	colorCells[i] = display.newRect(colorBox.x-(cw/15)*5+41*i,cw/15*1.5-12,35,35)
+	colorCells[i]:setFillColor(unpack(v))
+	colorCells[i].anchorX=1;colorCells[i].anchorY=0.5
+	colorCells[i].color=v
+	colorCells[i].touch = selColor
+	colorCells[i]:addEventListener("touch",colorCells[i])
+end
+
 -- Alter rectangle
 function moveRectP3(rect,xo,yo,x,y)
 	rect.path.x3=x-xo
@@ -57,6 +89,11 @@ function moveRectP3(rect,xo,yo,x,y)
 
 	rect.path.x4=x-xo
 	rect.path.y2=y-yo
+end
+function alterRadius(circ,xo,yo,xi,yi)
+	x=math.pow(xi-xo,2)
+	y=math.pow(yi-yo,2)
+	circ.path.radius = math.sqrt(x+y)
 end
 
 -- Canva actions
@@ -72,8 +109,8 @@ function canvas:touch(event)
 		end
 		if(event.phase== "moved") then
 			line:append(event.x,event.y)
-			print("x "..event.x,"y "..event.y)
 		end
+		return
 	elseif (idTool==2) then
 		if event.phase=="began" then
 			x1 = event.x
@@ -84,40 +121,68 @@ function canvas:touch(event)
 		end
 		if(event.phase== "moved") then
 			line:append(event.x,event.y)
-			print("x "..event.x,"y "..event.y)
 		end
+		return
 	elseif (idTool==3) then
 		if event.phase=="began" then
 			x1 = event.x
 			y1 = event.y
 			cuadrado = display.newRect(grp_drawings,x1,y1,50,50)
-			cuadrado.strokeWidth=Strok
+			cuadrado.strokeWidth=strok
 			cuadrado:setStrokeColor(unpack(baseColor))
-			cuadrado:setFillColor(unpack(baseColor))
+			cuadrado:setFillColor(unpack(Backcolor))
 			cuadrado.anchorX = 1 ; cuadrado.anchorY=1
 		end
 		if(event.phase== "moved") then
 			moveRectP3(cuadrado,x1,y1,event.x,event.y)
 		end
+		return
 	elseif (idTool==4) then
 		if event.phase=="began" then
 			x1 = event.x
 			y1 = event.y
-			circulo = display.newCircle(grp_drawings,x1,y1,25)
-			circulo.strokeWidth=8
-			circulo:setStrokeColor(0,0,0)
-			circulo:setFillColor(unpack(baseColor))
+			circulo = display.newCircle(grp_drawings,x1,y1,5)
+			circulo.strokeWidth=strok
+			circulo:setStrokeColor(unpack(baseColor))
+			circulo:setFillColor(unpack(Backcolor))
 			circulo.anchorX = 0 ; circulo.anchorY=0
 		end
 		if(event.phase== "moved") then
-			print(event.x)
+			alterRadius(circulo,x1,y1,event.x,event.y)
 		end
+		return
+	elseif (idTool==5) then
+		if event.phase=="began" then
+			x1 = event.x
+			y1 = event.y
+			vertex = {0,18 ,24,18 ,12,0}
+			triangulo = display.newPolygon(grp_drawings,x1,y1,vertex)
+			triangulo.strokeWidth=strok
+			triangulo:setStrokeColor(unpack(baseColor))
+			triangulo:setFillColor(unpack(Backcolor))
+			triangulo.anchorX = 0.5 ; triangulo.anchorY=0.5
+		end
+		if(event.phase== "moved") then
+			if x1 > cw/2 then
+				triangulo.xScale = (x1/event.x)*6
+			else
+				triangulo.xScale = (event.x/x1)*6
+			end
+			if y1 > ch/2 then
+				triangulo.yScale = (y1/event.y)*6
+			else
+				triangulo.yScale = (event.y/y1)*6
+			end
+
+		end
+		return
 	end
 
 	return
 end
 
 canvas:addEventListener("touch",canvas)
+
 
 -- cuadrado = display.newRect(cw/2, ch/2,50,50)
 -- 			cuadrado.strokeWidth=5
