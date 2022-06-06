be9e932e3b55b46affbadf5966b9535593c2aaa3
Miguel Frade Flores <40034087+miguelfradeflores@users.noreply.github.com>
2022-06-06 19:52:47 -0400

    archivo page1.lua de la historieta

---
+++ page1.lua
@@ -0,0 +1,87 @@
+-----------------------------------------------------------------------------------------
+--
+-- page1.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+-- forward declarations and other locals
+local background 
+-- function to show next animation
+
+-- touch event listener for background object
+
+
+function scene:create( event )
+	local sceneGroup = self.view
+
+	-- Called when the scene's view does not exist.å
+	-- 
+	-- INSERT code here to initialize the scene
+	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+	
+	-- create background image
+	background = display.newImageRect( sceneGroup, "batman5.jpg", display.contentWidth, display.contentHeight )
+	background.anchorX = 0
+	background.anchorY = 0
+	background.x, background.y = 0, 0
+	background.alpha = 1
+	
+
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+		
+	end	
+
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+		
+
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end		
+
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
